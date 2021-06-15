c3a3ae69de5e41e4d5cd3c7be346cc658518c8e5
DrgnInventor <62597889+DrgnInventor@users.noreply.github.com>
2021-06-15 15:26:14 +0300

    OOP for db creation

--- db.php
+++ db.php
@@ -1,41 +1,54 @@
 <?php
-$serverName = "localhost";
-$userName = "";
-$passwrd = "";
-$dbName = "doApp";
+class DataBase {
+    private $serverName = "localhost";
+    private $userName = "";
+    private $passwrd = "";
+    private $dbName = "doApp";
+    private $sqlFile = "mysql_todos_table.txt";
 
-//Establish connection to mysql server
-$connect = new mysqli($serverName, $userName, $passwrd);
-if ($connect -> connect_error) {
-    die("Connection failed: " . $connect->connect_error);
+    public function __construct($serverName, $userName, $passwrd, $dbName, $sqlFile)
+    {
+        $this->serverName = $serverName;
+        $this->userName = $userName;
+        $this->passwrd = $passwrd;
+        $this->dbName = $dbName;
+        $this->sqlFile = $sqlFile;
+        $this->createDB();
+        $this->connectDb();
+        $this->readSqlQuery();
+        $this->createTable();
+
+    }
+    private function connectDb(){
+        $connect = new mysqli($this->serverName, $this->userName, $this->passwrd);
+        if ($connect -> connect_error) {
+            die("Connection failed: " . $connect->connect_error);
+            }  
+        return $connect;
+    }
+    
+    private function createDB(){
+        $con = $this->connectDb();
+        $sqlDB = "CREATE DATABASE IF NOT EXISTS $this->dbName";
+        if ($con->query($sqlDB) === false) {
+            echo "Error creating database: " . $con->connect_error;
+        }
+    }
+
+    private function readSqlQuery(){
+        $fContent = fopen($this->sqlFile, 'r');
+        $sql = fread($fContent, filesize($this->sqlFile));
+        fclose($fContent);
+        return $sql;
+    }
+
+    private function createTable(){
+        $con = $this->connectDb();
+        $con->hostname = $this->dbName;
+        if ($con->query($this->readSqlQuery()) === False){
+            echo "Error creating table: " . $con->error;
+        }
+    }
+
 }
-
-//Create DATABASE doApp
-$sqlDB = "CREATE DATABASE IF NOT EXISTS $dbName";
-if ($connect->query($sqlDB) === false) {
-    echo "Error creating database: " . $connect->connect_error;
-} elseif ($connect->query($sqlDB) === true){
-    echo "Database Created ";
-} else {
-    echo "Something broke ";
-}
-
-$connect = new mysqli($serverName, $userName, $passwrd, $dbName);
-if ($connect -> connect_error) {
-    die("Connection failed: " . $connect->connect_error);
-}
-
-//Reads sql querys from txt file
-$sqlFile = 'mysql_todos_table.txt';
-
-$fContent = fopen($sqlFile, 'r');
-$sql = fread($fContent, filesize($sqlFile));
-fclose($fContent);
-
-//Creates table
-if ($connect->query($sql) === False){
-    echo "Error creating table: " . $connect->error;
-}
-
-mysqli_close($connect);
 ?>
