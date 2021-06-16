4e5282b666173a71e062de5126c1ffe723e61a4b
DrgnInventor <62597889+DrgnInventor@users.noreply.github.com>
2021-06-16 14:17:02 +0300

    reorganizing

--- page_elements/navBar.php
+++ page_elements/navBar.php
@@ -2,6 +2,6 @@
   <a class="active" href="index.php">To Do</a>
   <a href="completedTasks.php">Completed</a>
   <a href="AddTodo.php">Add</a>
-  <script src="userServerInteraction.js"></script>
+  <script src="js/userServerInteraction.js"></script>
   <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
 </div>
